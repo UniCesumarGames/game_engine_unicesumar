a5b4478d92e4098e8b949b36339152c908a88372
José Magalhães <joses.magalhaes@hotmail.com>
2025-11-25 22:00:44 -0300

    :construction: Finalizacao das animacoes do Player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,13 @@ public class PlayerMovement : MonoBehaviour
     public float velocidade = 5f;
     private Rigidbody2D rb;
     private Vector2 movimento;
+
+    private Animator anim;
+    private bool isWalking = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
 
@@ -22,6 +26,15 @@ public class PlayerMovement : MonoBehaviour
         if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed) movimento.x = -1;
 
         movimento = movimento.normalized;
+
+        isWalking = (movimento.x !=0 || movimento.y !=0);
+        anim.SetBool("isWalking", isWalking);
+
+        if (isWalking)
+        {
+            anim.SetFloat("eixoX", movimento.x);
+            anim.SetFloat("eixoY", movimento.y);
+        }
     }
 
     void FixedUpdate()
